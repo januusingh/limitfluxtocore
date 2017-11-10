014ca43696cd1e7117c5b5a0fe2bac6c191539d4
januusingh <jahnavis@berkeley.edu>
2017-11-10 09:21:45 -0800

    completed tests

--- tests/testLFTC.py
+++ tests/testLFTC.py
@@ -20,12 +20,25 @@ def test_limitFluxToCore():
     assert(len(nonZeroReactions)) == 2
     with pytest.raises(AssertionError):
         len(nonZeroReactions) != 2
-    assert nonZeroReactions[0] == pytest.approx(6.0519877877298019e-32, abs=0.001e-32), 'reaction flux different'
+    assert nonZeroReactions[0] == pytest.approx(6.0519877877298019e-32, abs=0.001e-32), 'Different Reaction Fluxes'
     with pytest.raises(AssertionError):
         nonZeroReactions[0]!= pytest.approx(6.0519877877298019e-32, abs=0.001e-32)
-    assert nonZeroReactions[1]) == pytest.approx(9.8842775465710206e-16, 'reaction flux different'
-    assert fluxIntoCore == pytest.approx(1.1372958348092502e-15), 'flux into core different'
-    assert(len(consumingFluxes)) == 48
-    assert(len(producingFLuxes)) == 194
-    assert(sum(producingFluxes)) == 9.8842775465710206e-16
-    assert(sum(producingFluxes) - sum(consumingFluxes)== fluxIntoCore)
+    assert nonZeroReactions[1] == pytest.approx(9.8842775465710206e-16, abs=0.001e-16),'Different Reaction Fluxes'
+    with pytest.raises(AssertionError):
+        nonZeroReactions[1]!= pytest.approx(9.8842775465710206e-16, abs=0.001e-16)
+    assert fluxIntoCore == pytest.approx(1.1372958348092502e-15, abs=0.001e-15), 'Different fluxIntoCore'
+    with pytest.raises(AssertionError):
+        fluxIntoCore != pytest.approx(1.1372958348092502e-15, abs=0.001e-15)
+    assert len(consumingFluxes) == 48, 'Different consumingFlux'
+    with pytest.raises(AssertionError):
+        len(consumingFluxes) != 48
+    assert len(producingFLuxes) == 194, 'Different producingFlux'
+    with pytest.raises(AssertionError):
+        len(producingFluxes) != 194
+    assert sum(producingFluxes) == pytest.approx(9.8842775465710206e-16, abs=0.001e-16), 'Different producingFlux'
+    with pytest.raises(AssertionError):
+        sum(producingFluxes) != pytest.approx(9.8842775465710206e-16, abs=0.001e-16)
+    producingMinusConsumingFlux = sum(producingFluxes) - sum(consumingFluxes)
+    assert producingMinusConsumingFlux == pytest.approx(fluxIntoCore, abs=0.001e-15), 'Incorrect FluxIntoCore'
+    with pytest.raises(AssertionError):
+        producingMinusConsumingFlux != pytest.approx(fluxIntoCore, abs=0.001e-15)
